00692bc90a4cebc4516413e6d8afd0834de33b2b
Saeed Nemati <sn.1361@gmail.com>
2021-06-18 23:20:53 +0430

    working

--- setup.sh
+++ setup.sh
@@ -3,21 +3,23 @@
 echo "running setup from /nefcanto/infra"
 echo "directory is: " $PWD
 
+export Package="$(basename $PWD)"
+
 # package & app = module & host = reusable & runnable
 
 sudo rm -rf /setup
 
 sudo mkdir /setup
 
-if [ -f "Routes.js "] then
+if [ -f "Routes.js" ]; then
     echo "React, reusable"
-    sudo wget https://raw.githubusercontent.com/HolismReact/Infra/main/docker-compose.yml
+    sudo wget -O /setup/docker-compose.yml https://raw.githubusercontent.com/HolismReact/Infra/main/docker-compose.yml
     docker-compose -f /setup/docker-compose.yml up
-elif [ -f "composer.json" ] && [ ! -f ".env.example" ] then
+elif [ -f "composer.json" ]; then
     echo "Laravel, reusable"
-elif [ -f "composer.json" ] && [ -f ".env.example" ] then
+elif [ -f ".env" ]; then
     echoe "Laravel, runnable"
-elif [ -f "package.json" ] then
+elif [ -f "package.json" ]; then
     echo "React, runnable"
 else
     echo "None"
@@ -27,4 +29,5 @@ fi
 # sudo chmod +x /nefcanto/infra/setup.sh"
 
 # then register it globally
+# sudo chmod -R 777 /etc/bash.bashrc
 # sudo echo "alias setup='/nefcanto/infra/setup.sh .'" >> /etc/bash.bashrc
