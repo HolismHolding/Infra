20023cb7e5ec2c603eb9f5c3e51bb25dbe24376c
Saeed Nemati <sn.1361@gmail.com>
2021-06-18 22:36:45 +0430

    creatin multi-purpose setup

--- setup.sh
+++ setup.sh
@@ -3,9 +3,25 @@
 echo "running setup from /nefcanto/infra"
 echo "directory is: " $PWD
 
-docker-compose -f /nefcanto/infra/docker-compose.yml build $PWD
-docker-compose up -d
-docker-compose logs --f db
+# package & app = module & host = reusable & runnable
+
+sudo rm -rf /setup
+
+sudo mkdir /setup
+
+if [ -f "Routes.js "] then
+    echo "React, reusable"
+    sudo wget https://raw.githubusercontent.com/HolismReact/Infra/main/docker-compose.yml
+    docker-compose -f /setup/docker-compose.yml up
+elif [ -f "composer.json" ] && [ ! -f ".env.example" ] then
+    echo "Laravel, reusable"
+elif [ -f "composer.json" ] && [ -f ".env.example" ] then
+    echoe "Laravel, runnable"
+elif [ -f "package.json" ] then
+    echo "React, runnable"
+else
+    echo "None"
+fi
 
 # make it executable
 # sudo chmod +x /nefcanto/infra/setup.sh"
