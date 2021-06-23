76bb74dd1def6c63e92519f01ebc00d883017e34
Saeed Nemati <sn.1361@gmail.com>
2021-06-23 14:21:29 +0430

    change

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 
 # somehow setup hosts too => https://www.interserver.net/tips/kb/local-domain-names-ubuntu/
 
-echo "running setup from /nefcanto/infra"
+echo "running nefcanto setup"
 echo "directory is: " $PWD
-
 export Package="$(basename $PWD)"
 
 # package & app = module & host = reusable & runnable
@@ -13,23 +12,24 @@ sudo rm -rf /setup
 
 sudo mkdir /setup
 
-if [ -f "Routes.js" ]; then
-    echo "React, reusable"
+if [ -f "App.js" ]; then
+    echo "React, panel"
+    sudo wget -0 /setup/docker-compose.yml https://raw.githubusercontent.com/HolismReact/Infra/main/panel-compose.yml
+    docker-compose -f /setup/docker-compose.yml up
+elif [ -f "Routes.js" ]; then
+    echo "React, reusable|package|module"
     sudo wget -O /setup/docker-compose.yml https://raw.githubusercontent.com/HolismReact/Infra/main/docker-compose.yml
     docker-compose -f /setup/docker-compose.yml up
 elif [ -f "composer.json" ]; then
-    echo "Laravel, reusable"
+    echo "Laravel, reusable|package|module"
 elif [ -f ".env" ]; then
-    echoe "Laravel, runnable"
+    echoe "Laravel, runnable|host|app"
 elif [ -f "package.json" ]; then
     echo "React, runnable"
 else
     echo "None"
 fi
 
-# make it executable
-# sudo chmod +x /nefcanto/infra/setup.sh"
-
-# then register it globally
-# sudo chmod -R 777 /etc/bash.bashrc
+# register it globally
+# sudo chmod u+rw /etc/bash.bashrc
 # sudo echo "alias setup='/nefcanto/infra/setup.sh .'" >> /etc/bash.bashrc
