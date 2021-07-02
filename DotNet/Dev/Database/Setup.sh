bb372ff953bf340fc515df53ac82442637511b76
Saeed Nemati <sn.1361@gmail.com>
2021-07-02 11:52:16 +0430

    Uploaded by script, no commit msg added

--- DotNet/Dev/Database/Setup.sh
+++ DotNet/Dev/Database/Setup.sh
@@ -1,10 +1,6 @@
 echo "alias dblogin='/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P lksU2o412f7tBj58t07B -d master'" >> /etc/bash.bashrc
 
-echo "creating database for $Runnable"
-
-rm -rf /temp/Script.sql
-
-echo "create database $Runnable" >> /temp/Script.sql
+echo "use master" >> /temp/Script.sql
 echo "go" >> /temp/Script.sql
 
 for i in {1..50};
@@ -12,7 +8,14 @@ do
     /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P lksU2o412f7tBj58t07B -d master -i /temp/Script.sql
     if [ $? -eq 0 ]
     then
-        echo "database $Runnable is created"
+        echo "SQL Server is ready"
+        if [ -f "/$RepositoryPath/SetupDatabase.sh" ]; then
+            /$RepositoryPath/SetupDatabase.sh
+        else
+            cd DataAccess
+            dotnet ef migrations update
+        fi
+        /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P lksU2o412f7tBj58t07B -d master -i /temp/Script.sql
         break
     else
         echo "waiting for SQL Server..."
