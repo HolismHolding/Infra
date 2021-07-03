97aaa34b89f18aa6f05e0622c6693dcc55e47949
Saeed Nemati <sn.1361@gmail.com>
2021-07-04 02:35:46 +0430

    permission check

--- Setup.sh
+++ Setup.sh
@@ -25,7 +25,12 @@ function MakeSureTempDirectoryExists() {
     if [ ! -d "/Temp" ]; then
         sudo mkdir /Temp
     fi
-    sudo chmod -R 777 /Temp
+    permissions=$(stat /Temp | grep -oP "(?<=Access: \()[^)]*")
+    if [  $permissions = "0777/drwxrwxrwx" ]; then
+        echo '/Temp folder has full access'
+    else 
+        sudo chmod -R 777 /Temp
+    fi 
 }
 
 function GetHoldingInfra(){
