ae9f6910b1d59194648dd2dcfc17190fd8a2dcb7
Saeed Nemati <sn.1361@gmail.com>
2021-07-05 15:07:15 +0430

    permissions

--- DotNet/Dev/Database/Setup.sh
+++ DotNet/Dev/Database/Setup.sh
@@ -6,13 +6,6 @@ do
     if [ $? -eq 0 ]
     then
         echo "SQL Server is ready"
-        if [ -f "/$RepositoryPath/SetupDatabase.sh" ]; then
-            /$RepositoryPath/SetupDatabase.sh
-        else
-            cd DataAccess
-            dotnet ef migrations update
-        fi
-        /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P lksU2o412f7tBj58t07B -d master -i /Temp/Script.sql
         break
     else
         echo "waiting for SQL Server..."
---
+++ DotNet/Dev/Setup.sh
@@ -0,0 +1,17 @@
+for i in {1..50};
+do
+    if [ $? -eq 0 ]
+    then
+        echo "SQL Server is ready"
+        if [ -f "/$RepositoryPath/SetupDatabase.sh" ]; then
+            /$RepositoryPath/SetupDatabase.sh
+        else
+            cd DataAccess
+            dotnet ef migrations update --project DataAccess
+        fi
+        break
+    else
+        echo "Waiting for SQL Server..."
+        sleep 1
+    fi
+done
