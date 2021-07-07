1a7d54a0f4144a4d8c00e2de1b3d2dcc4a70ab52
Saeed Nemati <sn.1361@gmail.com>
2021-07-07 21:41:14 +0430

    change

--- DotNet/Setup.sh
+++ DotNet/Setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-. /HolismHolding/Infra/CommonFunctions.sh
-
 function IsDotNet() {
     sln=$(find "$PWD" -name *.sln|head -n1)
     if [ ! -z "$sln" ]; then
@@ -52,6 +50,29 @@ function LinkGitIgnore() {
 function PullDockerImage() {
     echo 'Pulling docker image holism/dotnet-dev:latest'
     docker pull holism/dotnet-dev:latest
+}
+
+function GetDependencies() {
+    echo "Getting dependencies";
+    if [ ! -f "$PWD/Dependencies" ]; then
+        return;
+    fi
+    while read Dependency; do  
+        Org=$(echo $Dependency | cut -d'/' -f1)
+        Repo=$(echo $Dependency | cut -d'/' -f2)
+        if [ ! -d "/$Org" ]; then
+            sudo mkdir "/$Org"
+            sudo chmod -R 777 "/$Org"
+        fi
+        if [ ! -d "/$Org/$Repo" ]; then 
+            echo "Cloning /$Org/$Repo"
+            git -C /$Org clone git@github.com:$Org/$Repo
+        else 
+            echo "Pulling /$Org/$Repo"
+            git -C /$Org/$Repo pull
+        fi
+        volumes="$volumes\n            - *$Org*$Repo:*$Org*$Repo"
+    done <<< "$({ cat "$PWD/Dependencies"; echo; })"
 }
 
 function SetupDotNet() {
