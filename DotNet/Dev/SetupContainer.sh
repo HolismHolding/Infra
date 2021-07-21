bdddc172ac330261402209f6bd65f1e2067b5cf6
Saeed Nemati <sn.1361@gmail.com>
2021-07-21 13:19:57 +0430

    committed by script

--- DotNet/Dev/SetupContainer.sh
+++ DotNet/Dev/SetupContainer.sh
@@ -1,20 +1,13 @@
 echo "Path inside container is: $PWD"
 
-function RunMigrations() {
-    find / -type d -name Migrations 2>&- | grep DataAccess/Migrations |
-    while read migrationDirectory; do
-        dataAccess=$(dirname $migrationDirectory);
-        echo "Running migrations for $dataAccess";
-        dotnet ef database update --project $dataAccess &
-    done 
-}
+
 
 for i in {1..50};
 do
     if [ $? -eq 0 ]
     then
         echo "SQL Server is ready"
-        RunMigrations
+        #RunMigrations
         break
     else
         echo "Waiting for SQL Server..."
