cfd8bf7dc13cf0df0669465b2bf7755c9096ab69
Saeed Nemati <sn.1361@gmail.com>
2021-08-09 11:19:53 +0430

    committed by script

--- Server/CreateCertificate.sh
+++ Server/CreateCertificate.sh
@@ -1,10 +1,13 @@
-export MainHost="put-your-$MainHost-here";
+# https://www.nginx.com/blog/using-free-ssltls-certificates-from-lets-encrypt-with-nginx/
 
-certbot --nginx -d $MainHost.com \
-	-d www.$MainHost.com \
-	-d accounts.$MainHost.com \
-	-d user.$MainHost.com \
-	-d api.user.$MainHost.com \
-	-d admin.$MainHost.com \
-	-d api.admin.$MainHost.com
+export MainHost="freya.center"; \
+
+certbot --nginx -d $MainHost \
+	-d www.$MainHost \
+	-d accounts.$MainHost \
+	-d user.$MainHost \
+	-d api.user.$MainHost \
+	-d admin.$MainHost \
+	-d api.admin.$MainHost
     # add other roles' panels and apis here
+	
