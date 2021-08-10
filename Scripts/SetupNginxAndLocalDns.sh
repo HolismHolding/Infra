f0c3fcfec6da34bb65585f392e2a5842a034d4f9
Saeed Nemati <sn.1361@gmail.com>
2021-08-10 18:54:52 +0430

    committed by script

--- Scripts/SetupNginxAndLocalDns.sh
+++ Scripts/SetupNginxAndLocalDns.sh
@@ -9,6 +9,8 @@ function SetupNginxAndLocalDns() {
     if [ -f "/etc/nginx/conf.d/$Host.conf" ]; then
         rm -rf "/etc/nginx/conf.d/$Host.conf";
     fi
+    export host='$host';
+    export scheme='$scheme';
     envsubst < /HolismHolding/Infra/NginxReverseProxyTemplate > /etc/nginx/conf.d/$Host.conf
     sed -i 's/https:\/\/;/https:\/\/$server_name$request_uri;/g' /etc/nginx/conf.d/$Host.conf
     #systemctl reload nginx
