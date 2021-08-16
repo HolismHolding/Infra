d662b4bbea7ecdc4d8cb585915cef4c6ab7365ca
Saeed Nemati <sn.1361@gmail.com>
2021-08-16 11:02:40 +0430

    committed by script

--- Scripts/CreateCertificate.sh
+++ Scripts/CreateCertificate.sh
@@ -1,10 +1,14 @@
 function CreateCertificate() {
-    if [ -f "/Temp/$Organization/$Repository/Certificate.pem" ]; then
-        return;
-    fi
+    # if [ -f "/Temp/$Organization/$Repository/Certificate.pem" ]; then
+    #     return;
+    # fi
+    sudo rm -rf /Temp/$Organization/$Repository/Certificate.pem;
+    sudo rm -rf /Temp/$Organization/$Repository/Key.pem
     if [ -z ${Host+x} ]; then
         return;
     fi
     echo "Creating certificate for $Host";
     mkcert -cert-file /Temp/$Organization/$Repository/Certificate.pem -key-file /Temp/$Organization/$Repository/Key.pem $Host 2>/dev/null
+    sudo chmod 777 /Temp/$Organization/$Repository/Certificate.pem;
+    sudo chmod 777 /Temp/$Organization/$Repository/Key.pem;
 }
