8fdda3dea2ef2347a4856d6532a6e5f2a57fe1c0
Saeed Nemati <sn.1361@gmail.com>
2021-09-09 22:48:25 +0430

    committed by script

--- DotNet/Debug/Debug.sh
+++ DotNet/Debug/Debug.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# debug is better to work both inside .sln directory and .csproj directory
+# have a fallback of what project should be debugged, based on conventional naming (for example if none is provided, debug Api/Api.csproj)
+
+. /HolismHolding/Infra/Scripts/GetDotNetInfra.sh
+. /HolismHolding/Infra/Scripts/LinkGitIgnore.sh
+. /HolismHolding/Infra/Scripts/GetDependencies.sh
+
 function LinkDevContainer() {
     
-    if [ -d "$PWD/.devcontainer" ]; then
-        sudo rm -rf "$PWD/.devcontainer"
+    if [ -d "$PWD/$SelectedProject/.devcontainer" ]; then
+        sudo rm -rf "$PWD/$SelectedProject/.devcontainer"
     fi
 
-    sudo mkdir $PWD/.devcontainer
-    sudo chmod -R 777 $PWD/.devcontainer
-    sudo ln -s /HolismHolding/Infra/DotNet/Debug/devcontainer "$PWD/.devcontainer/devcontainer.json"
+    sudo mkdir $PWD/$SelectedProject/.devcontainer
+    sudo chmod -R 777 $PWD/$SelectedProject/.devcontainer
+    sudo ln -s /HolismHolding/Infra/DotNet/Debug/devcontainer "$PWD/$SelectedProject/.devcontainer/devcontainer.json"
 }
 
-
-function LinkVScodefiles() {
+function LinkVSCodeFiles() {
     if [ -d "$PWD/.vscode" ]; then
         sudo rm -rf "$PWD/.vscode"
     fi
@@ -28,18 +34,36 @@ function PullDotNetDockerImage() {
     docker pull holism/dotnet-debug:latest
 }
 
-GetDotNetInfra &
-GetDotNetAccounts &
-volumes=""
-GetDependencies volumes
-echo -e $volumes
-LinkGitIgnore $PWD
-# PullDotNetDockerImage    echo "DotNet, runnable|host|app"
-LinkDevContainer
-LinkVScodefiles
-ComposeFile=/Temp/$Organization/$Repository/debug.yml
-mkdir -p $(dirname $ComposeFile)
-envsubst < /HolismHolding/Infra/DotNet/Debug/debug.yml > $ComposeFile
-sed -i "s/VolumeMappingPlaceHolder/$volumes/g" $ComposeFile
-sed -i "s/*/\//g" $ComposeFile
-docker-compose -p "${Organization}_${Repository}" -f $ComposeFile up --remove-orphans
+function DebugDotNet() {
+
+    if [ -z "$SelectedProject" ]; then
+        if [ ! -d "$PWD/Api" ]; then
+            echo -e '\033[0;31m'"You should provide the project name to debug";
+            exit;
+        fi
+        if [ -f "$PWD/Api/Startup.cs" ]; then
+            echo -e '\033[0;31m'"You are debugging in the wrong directory";
+            exit;
+        fi
+        export SelectedProject=Api
+    fi
+
+    echo $SelectedProject;
+
+    exit;
+
+    GetDotNetInfra &
+    volumes=""
+    GetDependencies volumes
+    echo -e $volumes
+    LinkGitIgnore $PWD
+    # PullDotNetDockerImage    echo "DotNet, runnable|host|app"
+    LinkDevContainer
+    LinkVSCodeFiles
+    ComposeFile=/Temp/$Organization/$Repository/debug.yml
+    mkdir -p $(dirname $ComposeFile)
+    envsubst < /HolismHolding/Infra/DotNet/Debug/debug.yml > $ComposeFile
+    sed -i "s/VolumeMappingPlaceHolder/$volumes/g" $ComposeFile
+    sed -i "s/*/\//g" $ComposeFile
+    docker-compose -p "${Organization}_${Repository}" -f $ComposeFile up --remove-orphans
+}
