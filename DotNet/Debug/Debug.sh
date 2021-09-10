952de74a956bf14baaccb72d3ea6225c95d561a8
Saeed Nemati <sn.1361@gmail.com>
2021-09-10 15:39:31 +0430

    committed by script

--- DotNet/Debug/Debug.sh
+++ DotNet/Debug/Debug.sh
@@ -15,18 +15,18 @@ function LinkDevContainer() {
 
     sudo mkdir $PWD/$SelectedProject/.devcontainer
     sudo chmod -R 777 $PWD/$SelectedProject/.devcontainer
-    sudo ln -s /HolismHolding/Infra/DotNet/Debug/devcontainer "$PWD/$SelectedProject/.devcontainer/devcontainer.json"
+    sudo ln -s /HolismHolding/Infra/DotNet/Debug/DevContainer "$PWD/$SelectedProject/.devcontainer/devcontainer.json"
 }
 
 function LinkVSCodeFiles() {
-    if [ -d "$PWD/.vscode" ]; then
-        sudo rm -rf "$PWD/.vscode"
+    if [ -d "$PWD/$SelectedProject/.vscode" ]; then
+        sudo rm -rf "$PWD/$SelectedProject/.vscode"
     fi
-    sudo mkdir $PWD/.vscode
-    sudo chmod -R 777 $PWD/.vscode
+    sudo mkdir $PWD/$SelectedProject/.vscode
+    sudo chmod -R 777 $PWD/$SelectedProject/.vscode
     
-    sudo ln -s /HolismHolding/Infra/DotNet/Debug/launch "$PWD/.vscode/launch.json"
-    sudo ln -s /HolismHolding/Infra/DotNet/Debug/tasks "$PWD/.vscode/tasks.json"
+    sudo ln -s /HolismHolding/Infra/DotNet/Debug/Lunch "$PWD/$SelectedProject/.vscode/launch.json"
+    sudo ln -s /HolismHolding/Infra/DotNet/Debug/Tasks "$PWD/$SelectedProject/.vscode/tasks.json"
 }
 
 function PullDotNetDockerImage() {
@@ -60,9 +60,9 @@ function DebugDotNet() {
     # PullDotNetDockerImage    echo "DotNet, runnable|host|app"
     LinkDevContainer
     LinkVSCodeFiles
-    ComposeFile=/Temp/$Organization/$Repository/debug.yml
+    ComposeFile=/Temp/$Organization/$Repository/DockerCompose.yml
     mkdir -p $(dirname $ComposeFile)
-    envsubst < /HolismHolding/Infra/DotNet/Debug/debug.yml > $ComposeFile
+    envsubst < /HolismHolding/Infra/DotNet/Debug/DockerCompose.yml > $ComposeFile
     sed -i "s/VolumeMappingPlaceHolder/$volumes/g" $ComposeFile
     sed -i "s/*/\//g" $ComposeFile
     docker-compose -p "${Organization}_${Repository}" -f $ComposeFile up --remove-orphans
