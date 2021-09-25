aa16cea5f6b559a9d97e255d092675c6e5b83bb7
Saeed Nemati <sn.1361@gmail.com>
2021-09-25 15:32:59 +0330

    committed by script

--- Accounts/ExportRealm.sh
+++
@@ -1,6 +0,0 @@
-docker exec -it kc /opt/jboss/keycloak/bin/standalone.sh \
--Djboss.socket.binding.port-offset=100 -Dkeycloak.migration.action=export \
--Dkeycloak.migration.provider=singleFile \
--Dkeycloak.migration.realmName=my_realm \
--Dkeycloak.migration.usersExportStrategy=REALM_FILE \
--Dkeycloak.migration.file=/Temp/$RealName.json
