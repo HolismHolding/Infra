2327e73db8510cea50b9472ac70a67ff5f8706ea
Saeed Nemati <sn.1361@gmail.com>
2021-09-27 16:54:36 +0330

    Update DevelopmentEnvironment.sh

--- Setup/DevelopmentEnvironment.sh
+++ Setup/DevelopmentEnvironment.sh
@@ -17,10 +17,10 @@ function InstallChrome()
 {
     Write "Installing Google Chrome ..."
 
-    sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
     sudo apt-get update 
-    sudo apt-get install google-chrome-stable -y
+    #sudo apt upgrade
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg -i google-chrome-stable_current_amd64.deb
 
     Write "Installed Google Chrome"
 }
