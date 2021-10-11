ccd3146719fa65f88236c68f61b36bfecf19d217
Saeed Nemati <sn.1361@gmail.com>
2021-10-11 15:18:01 +0330

    committed by script

--- Server/CreateCertificate.sh
+++ Server/CreateCertificate.sh
@@ -4,4 +4,8 @@ export MainHost="freya.center"; \
 
 certbot --nginx -d freya.center -d www.freya.center -d accounts.freya.center -d owner.freya.center -d api.owner.freya.center -d admin.freya.center -d api.admin.freya.center -d stylist.freya.center -d api.stylist.freya.center
  # add other roles' panels and apis here
+
+# Setup this cron:
+# 0 12 * * * /usr/bin/certbot renew --quiet
+
 	
