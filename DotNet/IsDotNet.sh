3265d8c447b5d2fbf99d9ca884ad5dcffb10eb84
Saeed Nemati <sn.1361@gmail.com>
2021-12-07 15:11:14 +0330

    committed by script

--- DotNet/IsDotNet.sh
+++ DotNet/IsDotNet.sh
@@ -1,7 +1,8 @@
 function IsDotNet() {
     sln=$(find "$PWD" -maxdepth 1 -name *.sln|head -n1)
     csproj=$(find "$PWD" -maxdepth 1 -name *.csproj|head -n1)
-    if [ ! -z "$sln" ] || [ ! -z "$csproj" ]; then
+    settings=$(find "$PWD" -maxdepth 1 -name Settings.json|head -n1)
+    if [ ! -z "$sln" ] || [ ! -z "$csproj" ] || [ ! -z "$settings" ]; then
         return 0;
     else 
         return 1;
