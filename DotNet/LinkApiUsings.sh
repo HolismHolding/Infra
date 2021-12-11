77e8350d32006e23f49841269c65236f3374d2ef
Saeed Nemati <sn.1361@gmail.com>
2021-12-11 20:37:11 +0330

    committed by script

--- DotNet/LinkApiUsings.sh
+++ DotNet/LinkApiUsings.sh
@@ -1,4 +1,14 @@
+. /HolismHolding/Infra/Scripts/Message.sh
+
 function LinkApiUsings()
 {
-    sudo fn -s -f /HolismDotNet/Infra/Api/Usings.cs /$Organization/$Repository/Api/Usings.cs
+    Divide
+    Info "Linking API Usings.cs"
+    Divide
+
+    sudo ln -s -f /HolismDotNet/Infra/Api/Usings.cs /$Organization/$Repository/Api/Usings.cs
+
+    sudo ln -s -f /$Organization/Common/InfraUsings.cs /$Organization/$Repository/Api/InfraUsings.cs
+
+    sudo ln -s -f /$Organization/Common/${OrganizationPrefix}Usings.cs /$Organization/$Repository/Api/${OrganizationPrefix}Usings.cs
 }
