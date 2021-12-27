11f762bd9f8d444d64c169170fa63403c55b038c
Saeed Nemati <sn.1361@gmail.com>
2021-12-27 12:56:59 +0330

    committed by script

--- DotNet/LinkApiSolution.sh
+++ DotNet/LinkApiSolution.sh
@@ -2,7 +2,7 @@
 
 . /HolismHolding/Infra/Scripts/Message.sh
 
-function LinkLocalSecrets()
+function LinkApiSolution()
 {
     Info "Linking Api.sln ...";
     sudo ln -s -f /HolismHolding/Infra/DotNet/Api.sln /$Organization/$Repository/$Repository.sln
