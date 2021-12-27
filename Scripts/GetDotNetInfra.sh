2be2a8d13141e86e0bd22e6835dfd9216531ce20
Saeed Nemati <sn.1361@gmail.com>
2021-12-27 11:28:46 +0330

    committed by script

--- Scripts/GetDotNetInfra.sh
+++ Scripts/GetDotNetInfra.sh
@@ -3,7 +3,9 @@ function GetDotNetInfra() {
     if [ -d "$infraPath" ]; then
         echo "Pulling $infraPath"
         git -C $infraPath pull
-    else 
+    else
+        sudo mkdir -p /HolismDotNet
+        sudo chmod -R 777 /HolismDotNet
         echo "Cloning $infraPath"
         git -C /HolismDotNet clone git@github.com:HolismDotNet/Infra
     fi
