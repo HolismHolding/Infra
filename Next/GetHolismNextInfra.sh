91148175effa548b32f0424d81780bfb213d125d
Saeed Nemati <sn.1361@gmail.com>
2021-12-27 11:25:16 +0330

    committed by script

--- Next/GetHolismNextInfra.sh
+++ Next/GetHolismNextInfra.sh
@@ -6,7 +6,8 @@ function GetHolismNextInfra() {
     else
         echo "Cloning $infraPath"
         sudo mkdir -p /HolismNext/Infra
-        sudo chmod -R /HolismNext
+        sudo chmod -R 777 /HolismNext
         git -C /HolismNext clone git@github.com:HolismNext/Infra || git -C /HolismNext clone https://github.com/HolismNext/Infra
     fi
+    sudo chmod -R 777 /HolismNext
 }
