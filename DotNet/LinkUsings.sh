550e11ed03662a81bf080972fb621b3183e80109
Saeed Nemati <sn.1361@gmail.com>
2022-01-03 14:06:41 +0330

    committed by script

--- DotNet/LinkUsings.sh
+++ DotNet/LinkUsings.sh
@@ -15,19 +15,5 @@ function LinkUsings()
     Info "/$Organization/$Repository/Api/${OrganizationPrefix}Usings.cs"
     sudo ln -s -f /$Organization/Common/${OrganizationPrefix}Usings.cs /$Organization/$Repository/Api/${OrganizationPrefix}Usings.cs
 
-    echo ""
-
-    if [ ! -f "$PWD/Dependencies" ]; then
-        return;
-    fi
-    while read Dependency; do  
-        Org=$(echo $Dependency | cut -d'/' -f1)
-        Repo=$(echo $Dependency | cut -d'/' -f2)
-        if [ -d /$Org/$Repo/Business ]; then
-            Info "/$Org/$Repo/Business/Usings.cs"
-            sudo ln -s -f /HolismDotNet/Infra/Business/Usings.cs /$Org/$Repo/Business/Usings.cs
-        fi
-    done <<< "$({ cat "$PWD/Dependencies"; echo; })"
-
     Divide
 }
