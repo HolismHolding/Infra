d3fcbb7c84dff48d0439e62d4a66f91af7b79928
Saeed Nemati <sn.1361@gmail.com>
2022-01-08 19:37:15 +0330

    committed by script

--- Node/CreateHolismNodeDirectory.sh
+++
@@ -1,7 +0,0 @@
-function CreateHolismNodeDirectory() {
-    if [ ! -d "/HolismNode" ]; then
-        echo "Creating /HolismNode directory"
-        sudo mkdir /HolismNode
-        sudo chmod -R 777 /HolismNode
-    fi
-}
--- Node/IsNode.sh
+++
@@ -1,7 +0,0 @@
-function IsNode() {
-    if [ -f "IsNode" ]; then
-        return 0;
-    else 
-        return 1;
-    fi
-}
