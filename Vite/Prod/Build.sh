f7aaea063ca46b2c70e9f37942dde6383483262e
Saeed Nemati <sn.1361@gmail.com>
2022-06-08 15:21:20 +0430

    committed by script

---
+++ Vite/Prod/Build.sh
@@ -0,0 +1,46 @@
+. /HolismHolding/Infra/Vite/CreateHolismViteDirectory.sh
+. /HolismHolding/Infra/Vite/GetHolismViteInfra.sh
+. /HolismHolding/Infra/Scripts/CopyHolismHoldingInfra.sh
+. /HolismHolding/Infra/Scripts/CopyDependencies.sh
+. /HolismHolding/Infra/Scripts/CopyRepository.sh
+. /HolismHolding/Infra/Scripts/RemoveGitsDirectory.sh
+. /HolismHolding/Infra/Scripts/Message.sh
+
+function CopyHolismViteInfra() {
+    Info "Copying HolismVite/Infra ...";
+    if [ -d "/Build/HolismVite" ]; then
+        sudo rm -rf /Build/HolismVite
+    fi
+    mkdir -p /Build/HolismVite
+    cp -r /HolismVite/Infra /Build/HolismVite/Infra
+}
+
+function ReplaceSymlinksWithOriginalFiles()
+{
+    Info "Replacing symlinks with the original files ..."
+    echo
+    find /Build/$Organization/$Repository -type l | 
+    while read file; do 
+        OriginalPath=$(readlink -f $file)
+        Info "$file - $OriginalPath"
+        sudo rm -rf $file;
+        cp -r $OriginalPath $file
+        sudo chmod 777 $file
+    done
+}
+
+function BuildVite() {
+    echo "Building panel ..."
+
+    CreateHolismViteDirectory
+    GetHolismViteInfra
+
+    CopyHolismHoldingInfra
+    CopyHolismViteInfra
+    CopyDependencies
+    CopyRepository
+    RemoveGitsDirectory
+    Divide
+
+    envsubst < /HolismHolding/Infra/Vite/Prod/Dockerfile > $Dockerfile
+}
