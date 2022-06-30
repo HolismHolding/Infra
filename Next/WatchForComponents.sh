61da35e27b1edc62ed4ab8086b02767f5ac89df8
Saeed Nemati <sn.1361@gmail.com>
2022-06-30 14:20:25 +0430

    committed by script

--- Next/WatchForComponents.sh
+++ Next/WatchForComponents.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-function RefreshLink()
+function SetJsConfig()
 {
     ModifiedFile=$1
-    find /$Organization/$Repository/ -type l | grep -v node_modules | grep -v .gitignore |
-    while read LinkedFile; do 
-        OriginalPath=$(readlink -f $LinkedFile)
-        if [[ $OriginalPath == $ModifiedFile ]]; then
-            Temp=$(dirname $ModifiedFile)/wjagCcKD0uY859DianqQ.temp
-
-            echo "Modified file: $ModifiedFile"
-            echo "Linked file: $LinkedFile"
-            echo "Original path: $OriginalPath"
-            echo "Temp: $Temp"
-            cp $ModifiedFile $Temp
-            ln -s -f $Temp $LinkedFile
-            sleep 0.01
-            ln -s -f $ModifiedFile $LinkedFile
-            rm -rf $Temp
-        fi
-    done
 }
 
-inotifywait -m /HolismReact/ -r -e modify --format '%w%f' "$1" | 
+function SetExports()
+{
+    ModifiedFile=$1
+}
+
+inotifywait -m /$RepositoryPath/components -r --format '%w%f' "$1" | 
 while read ModifiedFile
 do
-    RefreshLink $ModifiedFile
+    SetJsConfig $ModifiedFile
+    SetExports $ModifiedFile
 done
