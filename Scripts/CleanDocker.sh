26742216598cbeaf6a48344c23e1726f731d75d5
Saeed Nemati <sn.1361@gmail.com>
2022-07-24 14:09:48 +0430

    committed by script

--- Scripts/CleanDocker.sh
+++ Scripts/CleanDocker.sh
@@ -1,8 +1,9 @@
 function CleanDocker() {
     Divide
     Info 'Cleaning Docker, pruning images, networks, containers ...';
-    docker container prune --force
-    docker image prune --force
-    docker network prune --force
+    docker container prune --force 1>/dev/null
+    docker image prune --force 1>/dev/null
+    docker network prune --force 1>/dev/null
+    Info 'Cleaned Docker'
     Divide
 }
